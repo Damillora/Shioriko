88ee6e094bf018eb314334326b26253938d66489
Damillora <damillora@damillora.com>
2021-05-11 12:56:17 +0700

    chore: remove unused embed

--- main.go
+++ main.go
@@ -6,12 +6,6 @@ import (
 	"github.com/Damillora/Shioriko/pkg/app"
 )
 
-//go:embed web/static
-var staticFiles embed.FS
-
-//go:embed web/template
-var templateFiles embed.FS
-
 func main() {
 	app.Initialize()
 	app.Start()
