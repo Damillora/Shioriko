ee2b7a50c3b98cdf2719b81c1fe11e21582a4171
Damillora <damillora@damillora.com>
2021-05-21 18:34:15 +0700

    fix: fix first time users logged in by mistake

--- web/app/src/stores.js
+++ web/app/src/stores.js
@@ -1,8 +1,10 @@
 import { writable } from "svelte/store";
 
-const storedToken = localStorage.getItem("apiToken");
+const storedToken = localStorage.getItem("apiToken") ?? "";
 
 export const token = writable(storedToken);
 token.subscribe(value => {
-    localStorage.setItem("apiToken", value);
+    if (value != null) {
+        localStorage.setItem("apiToken", value);
+    }
 });
