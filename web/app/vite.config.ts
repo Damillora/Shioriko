f4aaf114ffa701a328ed7beb90b722a1ac945251
Damillora <developer@damillora.com>
2025-02-23 07:15:31 +0000

    chore: remove local dev env on vite config

--- web/app/vite.config.ts
+++ web/app/vite.config.ts
@@ -11,14 +11,4 @@ export default defineConfig({
       },
     },
   },
-  server: {
-    allowedHosts: ["5173.shinosawa.nanao.moe"],
-    proxy: {
-      // string shorthand:
-      // http://localhost:5173/foo
-      //   -> http://localhost:4567/foo
-      '/api': 'http://localhost:5050',
-      '/data': 'https://booru.nanao.moe',
-    }
-  }
 });
